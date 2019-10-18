774a7723c2d259eee82cf3989f1f0a9c5d59bbe8
Kyu-Young Lee <itdev43@kyowon.co.kr>
2019-10-18 11:37:20 +0900

    공통 Facade Controller 추가

---
+++ src/com/tistory/opensrc/basicservlet/controller/FacadeController.java
@@ -0,0 +1,14 @@
+package com.tistory.opensrc.basicservlet.controller;
+
+import javax.servlet.http.HttpServlet;
+
+public class FacadeController extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1110797199945908854L;
+
+	
+	
+}
